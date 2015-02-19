e63453f62867b11c77a7fd01b725051230d2abc8
Yoshiaki Rikitake <yoshiaki@sendai-nct.ac.jp>
2015-02-19 21:46:01 +0900

    move FARule to fa_rule.rb

--- dfa.rb
+++ dfa.rb
@@ -1,16 +1,5 @@
-class FARule < Struct.new(:state, :character, :next_state)
-  def applies_to?(state, character)
-    self.state == state && self.character == character
-  end
-  
-  def follow
-    next_state
-  end
+# require_relative 'fa_rule'
 
-  def inspect
-    "#<FARule #{state.inspect} --#{character.inspect}--> #{next_state.inspect}>"
-  end
-end
 
 class DFARulebook < Struct.new(:rules)
   def next_state(state, character)
