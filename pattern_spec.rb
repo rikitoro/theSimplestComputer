e8758123b712a1f654ddcfb4f45edc2fb3647ad8
Yoshiaki Rikitake <yoshiaki@sendai-nct.ac.jp>
2015-02-27 10:11:43 +0900

    add pattern.treetop

---
+++ pattern_spec.rb
@@ -0,0 +1,26 @@
+require 'rspec'
+require 'treetop'
+require_relative 'regex'
+
+Treetop.load('pattern')
+
+describe "PatternPaser" do
+  context "parse '(a(|b))*'" do
+    before do
+      parse_tree = PatternParser.new.parse('(a(|b))*')
+      @pattern = parse_tree.to_ast
+    end
+
+    it "to /(a(|b))*/" do
+      expect(@pattern.inspect).to eq '/(a(|b))*/'
+    end
+
+    it "matches 'abaab'" do
+      expect(@pattern).to be_matches 'abaab'
+    end
+
+    it "Not match 'abba'" do
+      expect(@pattern).not_to be_matches 'abba'
+    end    
+  end
+end
